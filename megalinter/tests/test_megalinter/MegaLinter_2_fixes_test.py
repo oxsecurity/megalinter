18b274326c952f73f554bd22f6619039693c763b
nvuillam <nvuillam@users.noreply.github.com>
2020-11-01 20:30:23 +0000

    [Mega-Linter] Apply linters fixes

--- megalinter/tests/test_megalinter/MegaLinter_2_fixes_test.py
+++ megalinter/tests/test_megalinter/MegaLinter_2_fixes_test.py
@@ -12,42 +12,47 @@ from megalinter.tests.test_megalinter.helpers import utilstest
 class MegalinterFixesTest(unittest.TestCase):
     def setUp(self):
         utilstest.linter_test_setup(
-            {'sub_lint_root': f'{os.path.sep}.automation{os.path.sep}test{os.path.sep}sample_project_fixes'})
+            {
+                "sub_lint_root": f"{os.path.sep}.automation{os.path.sep}test{os.path.sep}sample_project_fixes"
+            }
+        )
 
     def test_1_apply_fixes_on_one_linter(self):
-        super_linter, output = utilstest.call_super_linter({
-            'APPLY_FIXES': 'JAVASCRIPT_ES',
-            'ENABLE': 'JAVASCRIPT',
-            'ENABLE_LINTERS': 'JAVASCRIPT_ES',
-            'LOG_LEVEL': 'DEBUG',
-            'MULTI_STATUS': 'false'
-        })
-        self.assertTrue(len(super_linter.linters) > 0,
-                        "Linters have been created and run")
-        self.assertIn('Linting [JAVASCRIPT] files', output)
-        utilstest.assert_file_has_been_updated('javascript_for_fixes_1.js', True, self)
-        utilstest.assert_file_has_been_updated('env_for_fixes_1.env', False, self)
+        super_linter, output = utilstest.call_super_linter(
+            {
+                "APPLY_FIXES": "JAVASCRIPT_ES",
+                "ENABLE": "JAVASCRIPT",
+                "ENABLE_LINTERS": "JAVASCRIPT_ES",
+                "LOG_LEVEL": "DEBUG",
+                "MULTI_STATUS": "false",
+            }
+        )
+        self.assertTrue(
+            len(super_linter.linters) > 0, "Linters have been created and run"
+        )
+        self.assertIn("Linting [JAVASCRIPT] files", output)
+        utilstest.assert_file_has_been_updated("javascript_for_fixes_1.js", True, self)
+        utilstest.assert_file_has_been_updated("env_for_fixes_1.env", False, self)
 
     def test_2_apply_fixes_on_all_linters(self):
-        super_linter, output = utilstest.call_super_linter({
-            'APPLY_FIXES': 'all',
-            'LOG_LEVEL': 'DEBUG',
-            'MULTI_STATUS': 'false'
-        })
-        self.assertTrue(len(super_linter.linters) > 0,
-                        "Linters have been created and run")
-        self.assertIn('Linting [JAVASCRIPT] files', output)
+        super_linter, output = utilstest.call_super_linter(
+            {"APPLY_FIXES": "all", "LOG_LEVEL": "DEBUG", "MULTI_STATUS": "false"}
+        )
+        self.assertTrue(
+            len(super_linter.linters) > 0, "Linters have been created and run"
+        )
+        self.assertIn("Linting [JAVASCRIPT] files", output)
         # Markdown lint fix (disabled while it does not work)
         # utilstest.assert_file_has_been_updated('markdown_for_fixes_1.md', True, self)
         # eslint fix
-        utilstest.assert_file_has_been_updated('javascript_for_fixes_1.js', True, self)
+        utilstest.assert_file_has_been_updated("javascript_for_fixes_1.js", True, self)
         # dotenv-linter fix
-        utilstest.assert_file_has_been_updated('env_for_fixes_1.env', True, self)
+        utilstest.assert_file_has_been_updated("env_for_fixes_1.env", True, self)
         # ktlint fix (format)
-        utilstest.assert_file_has_been_updated('kotlin_for_fixes_1.kt', True, self)
+        utilstest.assert_file_has_been_updated("kotlin_for_fixes_1.kt", True, self)
         # rubocop fix
-        utilstest.assert_file_has_been_updated('ruby_for_fixes_1.rb', True, self)
+        utilstest.assert_file_has_been_updated("ruby_for_fixes_1.rb", True, self)
         # black fix
-        utilstest.assert_file_has_been_updated('python_for_fixes_1.py', True, self)
+        utilstest.assert_file_has_been_updated("python_for_fixes_1.py", True, self)
         # npm-groovy-lint fix (disabled while it does not work)
         # utilstest.assert_file_has_been_updated('groovy_for_fixes_1.groovy', True, self)
