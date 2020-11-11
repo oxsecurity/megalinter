7bfde17290002f531bbdea8fbd2fe116935bbee9
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-11-11 13:48:23 +0100

    debug (2)

--- megalinter/MegaLinter.py
+++ megalinter/MegaLinter.py
@@ -305,7 +305,7 @@ class Megalinter:
                         os.path.join(dirpath, file) for file in sorted(filenames)
                     ]
             logging.debug(
-                "All found files before filtering:\n" + all_files.join(os.linesep)
+                "All found files before filtering:\n" + os.linesep.join(all_files)
             )
         # Filter files according to fileExtensions, fileNames , filterRegexInclude and filterRegexExclude
         if len(self.file_extensions) > 0:
