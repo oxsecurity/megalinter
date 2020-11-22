7b8c8dcc63fbe58ffa49ab9bc056f68340fc05e2
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-11-22 13:52:49 +0100

    Apply black reformmatting

--- megalinter/linters/PowershellLinter.py
+++ megalinter/linters/PowershellLinter.py
@@ -9,10 +9,9 @@ from megalinter import Linter
 
 
 class PowershellLinter(Linter):
-
     def __init__(self, params=None, linter_config=None):
         super(PowershellLinter, self).__init__(params, linter_config)
-        self.cli_executable = "powershell" if sys.platform == 'win32' else 'pwsh'
+        self.cli_executable = "powershell" if sys.platform == "win32" else "pwsh"
         self.cli_executable_help = self.cli_executable
         self.cli_executable_version = self.cli_executable
 
@@ -20,18 +19,22 @@ class PowershellLinter(Linter):
     def build_lint_command(self, file=None):
         pwsh_script = ["Invoke-ScriptAnalyzer -EnableExit"]
         if self.config_file is not None:
-            pwsh_script[0] += ' -Settings ' + self.config_file
-        pwsh_script[0] += ' -Path ' + file
-        cmd = [self.cli_executable,
-               '-NoProfile',
-               '-NoLogo',
-               '-Command', '\n'.join(pwsh_script)
-               ]
+            pwsh_script[0] += " -Settings " + self.config_file
+        pwsh_script[0] += " -Path " + file
+        cmd = [
+            self.cli_executable,
+            "-NoProfile",
+            "-NoLogo",
+            "-Command",
+            "\n".join(pwsh_script),
+        ]
         return cmd
 
     # Build the CLI command to get linter version
     def build_version_command(self):
-        cmd = [self.cli_executable_version,
-               '-Command', 'Write-Output $PsVersionTable.PsVersion;'
-               ]
+        cmd = [
+            self.cli_executable_version,
+            "-Command",
+            "Write-Output $PsVersionTable.PsVersion;",
+        ]
         return cmd
