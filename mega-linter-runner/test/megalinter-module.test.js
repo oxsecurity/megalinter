c5c96dd8cfaf7d4b4d8effdd5792c0f5ea8bf819
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-11-25 01:35:35 +0100

    Apply linters fixes

--- mega-linter-runner/test/megalinter-module.test.js
+++ mega-linter-runner/test/megalinter-module.test.js
@@ -1,17 +1,15 @@
 #! /usr/bin/env node
-"use strict";
-const { MegaLinterRunner } = require("../lib/index");
-let assert = require("assert");
+'use strict'
+const { MegaLinterRunner } = require('../lib/index')
+const assert = require('assert')
 
-describe("Module", function () {
-
-
-    it("(Module) run on own code base", async () => {
-        const options = {
-            path: "./..",
-            debug: true
-        }
-        const result = await new MegaLinterRunner().run(options)
-        assert(process.exitCode === 0, `process.exitCode is 0 (${process.exitCode} returned)`)
-    });
-});
+describe('Module', function () {
+  it('(Module) run on own code base', async () => {
+    const options = {
+      path: './..',
+      debug: true
+    }
+    const result = await new MegaLinterRunner().run(options)
+    assert(process.exitCode === 0, `process.exitCode is 0 (${process.exitCode} returned)`)
+  })
+})
