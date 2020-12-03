7a19c3e53e2e8ff36df55bce717c5664c22a7124
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-12-03 13:12:38 +0100

    try again

--- .automation/test/terraform_terrascan/bad/terraform_bad_1.tf
+++ .automation/test/terraform_terrascan/bad/terraform_bad_1.tf
@@ -1,8 +1,8 @@
-resource "aws_instanceee" "instanceWithNoVpc" {
-  amiwu           = "some-id"
-  instance_type = "t2.microblablabla"
+resource "aws_instance" "nico" {
+  ami           = "some-id"
+  instance_type = "t2.micro"
 
-  tagres = {
-    Namex = "HelloWorld"
+  tags = {
+    Name = "HelloWorld"
   }
 }
