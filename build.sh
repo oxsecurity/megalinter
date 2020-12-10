8fc9f4565cedde3ffd8328f57018bf898eb937b8
Sergey Kolomenkin <kolomenkin@gmail.com>
2020-12-10 17:29:25 +0300

    Update `build.sh` to prettify generated `search_index.json`
    It will minimize line differences in `search_index.json` and will make possible to review changes.

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+set -eu
 
 PYTHONPATH=.
 export PYTHONPATH
 ls
 python ./.automation/build.py
 mkdocs build
+
+# Prettify `search_index.json` after `mkdocs` to have better diffs in git.
+# `mkdocs` broke this few years ago: https://github.com/mkdocs/mkdocs/pull/1128
+python -m json.tool ./site/search/search_index.json > ./site/search/search_index_new.json
+mv -f -- ./site/search/search_index_new.json ./site/search/search_index.json
