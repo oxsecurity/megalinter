b9600e9b0bce175fe19ff663db5bcf86df6b3263
nvuillam <nvuillam@users.noreply.github.com>
2021-02-09 22:05:23 +0000

    [Mega-Linter] Apply linters fixes

--- mega-linter-runner/lib/options.js
+++ mega-linter-runner/lib/options.js
@@ -17,79 +17,80 @@ const optionator = require("optionator");
 
 // exports "parse(args)", "generateHelp()", and "generateHelpForOption(optionName)"
 module.exports = optionator({
-    prepend: "mega-linter [options]",
-    defaults: {
-        concatRepeatedArrays: true,
-        mergeRepeatedObjects: true
+  prepend: "mega-linter [options]",
+  defaults: {
+    concatRepeatedArrays: true,
+    mergeRepeatedObjects: true,
+  },
+  options: [
+    {
+      option: "release",
+      alias: "r",
+      type: "String",
+      default: "v4",
+      description: "Mega-Linter version",
+      example: ["stable", "latest", "v4.9.0"],
     },
-    options: [
-        {
-            option: "release",
-            alias: "r",
-            type: "String",
-            default: "v4",
-            description: "Mega-Linter version",
-            example: ["stable", "latest", "v4.9.0"]
-        },
-        {
-            option: "flavor",
-            type: "String",
-            default: "all",
-            description: "Mega-Linter flavor",
-            example: ["dotnet", "javascript", "java", "php", "python"]
-        },
-        {
-            option: "path",
-            alias: "p",
-            type: "path::String",
-            default: ".",
-            description: "Directory containing the files to lint (default: current directory)",
-            example: ["./path/to/my/files"]
-        },
-        {
-            option: "env",
-            alias: "e",
-            type: "[String]",
-            description: "Environment variable (multiple)",
-            example: [
-                "-e 'ENABLE=JAVASCRIPT' -e 'SHOW_ELAPSED_TIME=true'",
-                "-e 'ENABLE=JAVASCRIPT,YAML' -e 'DISABLE_LINTERS=MARKDOWN_MARKDOWN_LINK_CHECK'"]
-        },
-        {
-            option: "fix",
-            type: "Boolean",
-            description: "Apply formatters and fixes in linted sources"
-        },
-        {
-            option: "nodockerpull",
-            type: "Boolean",
-            description: "Do not pull docker image before running it"
-        },
-        {
-            option: "debug",
-            type: "Boolean",
-            description: "See debug logs"
-        },
-        {
-            option: "help",
-            alias: "h",
-            type: "Boolean",
-            description: "Show help (mega-linter --help OPTIONNAME to see option detail)"
-        },
-        {
-            option: "version",
-            alias: "v",
-            type: "Boolean",
-            description: "Show version"
-        },
-        {
-            option: "install",
-            alias: "i",
-            type: "Boolean",
-            description: "Generate Mega-Linter configuration in your project",
-        },
-    ],
-    mutuallyExclusive: [
-        ["help", "version", "install"]
-    ]
+    {
+      option: "flavor",
+      type: "String",
+      default: "all",
+      description: "Mega-Linter flavor",
+      example: ["dotnet", "javascript", "java", "php", "python"],
+    },
+    {
+      option: "path",
+      alias: "p",
+      type: "path::String",
+      default: ".",
+      description:
+        "Directory containing the files to lint (default: current directory)",
+      example: ["./path/to/my/files"],
+    },
+    {
+      option: "env",
+      alias: "e",
+      type: "[String]",
+      description: "Environment variable (multiple)",
+      example: [
+        "-e 'ENABLE=JAVASCRIPT' -e 'SHOW_ELAPSED_TIME=true'",
+        "-e 'ENABLE=JAVASCRIPT,YAML' -e 'DISABLE_LINTERS=MARKDOWN_MARKDOWN_LINK_CHECK'",
+      ],
+    },
+    {
+      option: "fix",
+      type: "Boolean",
+      description: "Apply formatters and fixes in linted sources",
+    },
+    {
+      option: "nodockerpull",
+      type: "Boolean",
+      description: "Do not pull docker image before running it",
+    },
+    {
+      option: "debug",
+      type: "Boolean",
+      description: "See debug logs",
+    },
+    {
+      option: "help",
+      alias: "h",
+      type: "Boolean",
+      description:
+        "Show help (mega-linter --help OPTIONNAME to see option detail)",
+    },
+    {
+      option: "version",
+      alias: "v",
+      type: "Boolean",
+      description: "Show version",
+    },
+    {
+      option: "install",
+      alias: "i",
+      type: "Boolean",
+      description: "Generate Mega-Linter configuration in your project",
+    },
+  ],
+  mutuallyExclusive: [["help", "version", "install"]],
 });
--- mega-linter-runner/test/megalinter-cli.test.js
+++ mega-linter-runner/test/megalinter-cli.test.js
@@ -1,37 +1,44 @@
 #! /usr/bin/env node
-'use strict'
-const assert = require('assert')
+"use strict";
+const assert = require("assert");
 const childProcess = require("child_process");
 const util = require("util");
 const exec = util.promisify(childProcess.exec);
 
-const release = process.env.MEGALINTER_RELEASE || 'insiders'
-const nodockerpull = (process.env.MEGALINTER_NO_DOCKER_PULL === 'true') ? true : false
+const release = process.env.MEGALINTER_RELEASE || "insiders";
+const nodockerpull =
+  process.env.MEGALINTER_NO_DOCKER_PULL === "true" ? true : false;
 
-const MEGA_LINTER = "mega-linter-runner "
+const MEGA_LINTER = "mega-linter-runner ";
 
-describe('CLI', function () {
-    it('(CLI) Show help', async () => {
-        const params = ["--help"];
-        const { stdout, stderr } = await exec(MEGA_LINTER + params.join(" "));
-        if (stderr) {
-            console.error(stderr);
-        }
-        assert(stdout, "stdout is set");
-        assert(stdout.includes("mega-linter [options]"), 'stdout contains help content');
-    })
+describe("CLI", function () {
+  it("(CLI) Show help", async () => {
+    const params = ["--help"];
+    const { stdout, stderr } = await exec(MEGA_LINTER + params.join(" "));
+    if (stderr) {
+      console.error(stderr);
+    }
+    assert(stdout, "stdout is set");
+    assert(
+      stdout.includes("mega-linter [options]"),
+      "stdout contains help content"
+    );
+  });
 
-    it('(CLI) Show version', async () => {
-        const params = ["--version"];
-        const { stdout, stderr } = await exec(MEGA_LINTER + params.join(" "));
-        if (stderr) {
-            console.error(stderr);
-        }
-        assert(stdout, "stdout is set");
-        assert(stdout.includes("mega-linter-runner version"), 'stdout should contains "mega-linter-runner version"');
-    })
+  it("(CLI) Show version", async () => {
+    const params = ["--version"];
+    const { stdout, stderr } = await exec(MEGA_LINTER + params.join(" "));
+    if (stderr) {
+      console.error(stderr);
+    }
+    assert(stdout, "stdout is set");
+    assert(
+      stdout.includes("mega-linter-runner version"),
+      'stdout should contains "mega-linter-runner version"'
+    );
+  });
 
-/*
+  /*
 Disabled until find a way to run with default options
     it('(CLI) Run installer', async () => {
         const params = ["--install"];
@@ -43,22 +50,22 @@ Disabled until find a way to run with default options
     })
 */
 
-    it('(CLI) run on own code base', async () => {
-        const params = [
-            "--path",
-            "./..",
-            "--release",
-            release,
-            "-e",
-            "\"ENABLE=YAML\""
-        ];
-        if (nodockerpull) {
-            params.push("--nodockerpull")
-        }
-        const { stdout, stderr } = await exec(MEGA_LINTER + params.join(" "));
-        if (stderr) {
-            console.error(stderr);
-        }
-        assert(stdout, "stdout is set");
-    }).timeout(600000)
-})
+  it("(CLI) run on own code base", async () => {
+    const params = [
+      "--path",
+      "./..",
+      "--release",
+      release,
+      "-e",
+      '"ENABLE=YAML"',
+    ];
+    if (nodockerpull) {
+      params.push("--nodockerpull");
+    }
+    const { stdout, stderr } = await exec(MEGA_LINTER + params.join(" "));
+    if (stderr) {
+      console.error(stderr);
+    }
+    assert(stdout, "stdout is set");
+  }).timeout(600000);
+});
