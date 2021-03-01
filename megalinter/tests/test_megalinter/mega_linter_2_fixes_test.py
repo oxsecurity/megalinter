a6cc437ea56f328942af26ac67041a2d310878c9
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2021-03-01 06:08:45 +0100

    Fix test classes

--- megalinter/tests/test_megalinter/mega_linter_2_fixes_test.py
+++ megalinter/tests/test_megalinter/mega_linter_2_fixes_test.py
@@ -29,7 +29,7 @@ class mega_linter_2_fixes(unittest.TestCase):
         self.assertTrue(
             len(mega_linter.linters) > 0, "Linters have been created and run"
         )
-        self.assertIn("### Processing [JAVASCRIPT] files", output)
+        self.assertIn("### Processed [JAVASCRIPT] files", output)
         time.sleep(5)
         utilstest.assert_file_has_been_updated("javascript_for_fixes_1.js", True, self)
         utilstest.assert_file_has_been_updated("env_for_fixes_1.env", False, self)
@@ -41,7 +41,7 @@ class mega_linter_2_fixes(unittest.TestCase):
         self.assertTrue(
             len(mega_linter.linters) > 0, "Linters have been created and run"
         )
-        self.assertIn("### Processing [JAVASCRIPT] files", output)
+        self.assertIn("### Processed [JAVASCRIPT] files", output)
         time.sleep(5)
         # Check fixable files has been updated
         fixable_files = [
