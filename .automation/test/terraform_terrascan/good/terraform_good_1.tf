d6980c1c129203b65c1099b01530238b7bf6fc82
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-05-10 23:50:20 +0200

    Fix terraform example

--- .automation/test/terraform_terrascan/good/terraform_good_1.tf
+++ .automation/test/terraform_terrascan/good/terraform_good_1.tf
@@ -1,12 +1,15 @@
 resource "aws_instance" "instanceWithVpc" {
   ami           = "some-id"
   instance_type = "t2.micro"
-
+  enable_monitoring = true 
   vpc_security_group_ids = ["sg-12345678901234567"]
-  subnet_id = "subnet-12345678901234567"
+  subnet_id              = "subnet-12345678901234567"
+
   metadata_options {
-    http_endpoint = "disabled"
+    http_tokens   = "required"
+    http_endpoint = "enabled"
   }
+
   tags = {
     Name = "HelloWorld"
   }
