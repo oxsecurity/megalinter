fc91dee71c50d938a6e25af467e0fcee4734b98c
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-05-30 15:18:58 +0200

    QuickFix sum of docker pulls

--- .automation/build.py
+++ .automation/build.py
@@ -1134,9 +1134,9 @@ def update_mkdocs_and_workflow_yml_with_flavors():
 def update_docker_pulls_counter():
     logging.info("Fetching docker pull counters on flavors images")
     total_count = 0
-    all_flavors = megalinter.flavor_factory.get_all_flavors().items()
-    all_flavors["all"] = {}
-    for flavor_id, _flavor_info in all_flavors:
+    all_flavors_ids = list(megalinter.flavor_factory.get_all_flavors().keys())
+    all_flavors_ids.insert(0,'all')
+    for flavor_id in all_flavors_ids:
         if flavor_id == "all":
             docker_image_url = (
                 "https://hub.docker.com/v2/repositories/nvuillam/mega-linter"
