540b0014176ed04e330b9d956172994e6e333281
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-08-28 18:44:11 +0200

    Better dump

--- megalinter/reporters/JsonReporter.py
+++ megalinter/reporters/JsonReporter.py
@@ -26,7 +26,9 @@ class JsonReporter(Reporter):
             self.is_active = True
 
     def produce_report(self):
-        resultJson = json.dumps(self.master, sort_keys=True, indent=4)
+        resultJson = json.dumps(
+            self.master, default=lambda o: o.__dict__, sort_keys=True, indent=4
+        )
         json_file_name = f"{self.report_folder}{os.path.sep}mega-linter-report.json"
         if config.get("JSON_REPORTER_FILE", "") != "":
             with open(json_file_name, "w", encoding="utf-8") as tap_file:
