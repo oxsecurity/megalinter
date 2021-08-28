c83f68ba374b02475ebc42feff6850c824176bd5
nvuillam <nvuillam@users.noreply.github.com>
2021-08-28 23:12:54 +0000

    [Mega-Linter] Apply linters fixes

--- megalinter/reporters/JsonReporter.py
+++ megalinter/reporters/JsonReporter.py
@@ -2,10 +2,10 @@
 """
 Output results in console
 """
-import jsonpickle
 import logging
 import os
 
+import jsonpickle
 from megalinter import Reporter, config
 
 
@@ -26,8 +26,12 @@ class JsonReporter(Reporter):
             self.is_active = True
 
     def produce_report(self):
-        resultJson = jsonpickle.encode(self.master,unpicklable =False,max_depth=3,indent=4)
-        json_file_name = f"{self.report_folder}{os.path.sep}"+config.get("JSON_REPORTER_FILE_NAME", "mega-linter-report.json")
+        resultJson = jsonpickle.encode(
+            self.master, unpicklable=False, max_depth=3, indent=4
+        )
+        json_file_name = f"{self.report_folder}{os.path.sep}" + config.get(
+            "JSON_REPORTER_FILE_NAME", "mega-linter-report.json"
+        )
         with open(json_file_name, "w", encoding="utf-8") as tap_file:
             tap_file.write(resultJson)
             logging.info(
