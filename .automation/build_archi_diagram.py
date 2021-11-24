14d4b9a3db7ebfa6ae3bbe938791036994aaf5cc
Laurent Laville <laurent.laville@gmail.com>
2021-11-24 10:24:10 +0100

    component to generate MegaLinter architecture diagram

---
+++ .automation/build_archi_diagram.py
@@ -0,0 +1,74 @@
+# !/usr/bin/env python3
+"""
+Generate architecture diagram of Mega-Linter
+See how to do it at https://kroki.io/#how
+"""
+# pylint: disable=import-error
+import requests
+import sys
+import base64
+import zlib
+import tempfile
+
+from string import Template
+
+class diagram:
+    GATEWAY_SERVER = "https://kroki.io"
+    DIAGRAM_TEMPLATE = "docs/assets/archi.blockdiag.txt"
+
+    def __init__(
+        self,
+        descriptor_language_count,
+        descriptor_format_count,
+        descriptor_tooling_format_count
+    ):
+        self.diagram_dict = dict(
+            language=str(descriptor_language_count),
+            format=str(descriptor_format_count),
+            tooling_format=str(descriptor_tooling_format_count)
+        )
+
+        self.build_diagram()
+
+    ### get binary image generated following format specified
+    def get_image_bin(self, format='svg', tempfile='kroki'):
+        image_content = self.run_query(
+            self.GATEWAY_SERVER,
+            'blockdiag',
+            format,
+            self.encoded_diagram
+        )
+
+        output_file = tempfile + '.' + format
+        with open(output_file, 'wb') as file:
+            file.write(image_content)
+
+        return output_file
+
+    ### get diagram textual description
+    def get_image_text(self):
+        return self.asset
+
+    ### generated image on gateway (default: online kroki.io server)
+    def run_query(self, gateway, diagram_type, diagram_format, encoded_diagram):
+        request = requests.get(f"{gateway}/{diagram_type}/{diagram_format}/{encoded_diagram}")
+        if request.status_code == 200:
+            return request.content
+        else:
+            raise Exception("Query failed to run by returning code of {}".format(request.status_code))
+
+    ### build diagram textual description and return deflate + base64 encoded version
+    def build_diagram(self):
+        with open(self.DIAGRAM_TEMPLATE, 'rt') as file:
+            diagram_source = file.read().rstrip()
+
+        self.asset = Template(diagram_source).safe_substitute(self.diagram_dict)
+
+        try:
+            temp = tempfile.NamedTemporaryFile(prefix="MegaLinter_")
+            temp.write(self.asset.encode())
+            temp.seek(0)
+            encoded_diagram = base64.urlsafe_b64encode(zlib.compress(temp.read(), 9))
+            self.encoded_diagram = encoded_diagram.decode()
+        finally:
+            temp.close()
