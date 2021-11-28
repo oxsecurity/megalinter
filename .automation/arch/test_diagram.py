71ee5e50be864de43aecbfa7f2fd6b727c10a91f
llaville <llaville@users.noreply.github.com>
2021-11-28 06:54:54 +0000

    [MegaLinter] Apply linters fixes

--- .automation/arch/test_diagram.py
+++ .automation/arch/test_diagram.py
@@ -7,4 +7,4 @@ from build_arch_diagram import BuildArchDiagram
 
 diag = BuildArchDiagram(29, 11, 13)
 print(diag.get_image_text())
-print(diag.get_image_file('svg', 'docs/assets/images/architecture'))
+print(diag.get_image_file("svg", "docs/assets/images/architecture"))
