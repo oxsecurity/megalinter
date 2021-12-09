eca19a940eb1cd2ccbeb8f459f7cdf8b77070b9f
Laurent Laville <laurent.laville@gmail.com>
2021-12-09 07:41:53 +0100

    for CI tests

---
+++ megalinter/linters/PhpLinter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Use PHP to lint php files
+https://www.php.net
+"""
+import logging
+import os
+import subprocess
+
+import megalinter
+
+
+class PhpLinter(megalinter.Linter):
+
+    # To execute before linting files
+    def before_lint_files(self):
+        php_version = config.get("PHP_VERSION", "7")
+        if php_version == "7":
+            return
+        pre_command = f"update-alternatives --set php /usr/bin/php{php_version}"
+        logging.debug("PHP before_lint_files: " + pre_command)
+        process = subprocess.run(
+            pre_command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            shell=True,
+        )
+        return_code = process.returncode
+        return_stdout = megalinter.utils.decode_utf8(process.stdout)
+        logging.debug(f"{return_code} : {return_stdout}")
