987bd2338b14ab0df826cb6a65b10fbc7de7cdac
nvuillam <nicolas.vuillamy@gmail.com>
2021-12-13 00:07:26 +0100

    Add workspace argument

--- megalinter/megalinter_server.py
+++ megalinter/megalinter_server.py
@@ -5,21 +5,30 @@ Start MegaLinter server
 import os
 import subprocess
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 print ("MegaLinter Server starting...")
 app = Flask(__name__)
 api = Api(app)
 
-subprocess_env = {**os.environ, "FORCE_COLOR": "0"}
+subprocess_env_default = {**os.environ, "FORCE_COLOR": "0"}
+
+parser = reqparse.RequestParser()
+parser.add_argument('workspace')
 
 class LintRequest(Resource):
     def post(self):
+        args = parser.parse_args()
+        if not "workspace" in args:
+            return {"errorMessage": "Missing workspace property"}
+        workspace = args["workspace"] 
+        print (f"Received request to lint workspace {args}")
         command = [
             "python",
             f"-m",
             "megalinter.run"
         ]
+        subprocess_env = {**subprocess_env_default, "DEFAULT_WORKSPACE": workspace}
         process = subprocess.run(
             command,
             stdout=subprocess.PIPE,
