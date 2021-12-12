06e76ba4cfeb0b62ca39115929e8d7ef215022e1
nvuillam <nicolas.vuillamy@gmail.com>
2021-12-12 23:56:30 +0100

    Server v0

--- megalinter/megalinter_server.py
+++ megalinter/megalinter_server.py
@@ -2,6 +2,8 @@
 """
 Start MegaLinter server
 """
+import os
+import subprocess
 from flask import Flask
 from flask_restful import Resource, Api
 
@@ -9,9 +11,26 @@ print ("MegaLinter Server starting...")
 app = Flask(__name__)
 api = Api(app)
 
+subprocess_env = {**os.environ, "FORCE_COLOR": "0"}
+
 class LintRequest(Resource):
     def post(self):
-        result = {'lint_request': 'done'}
+        command = [
+            "python",
+            f"-m",
+            "megalinter.run"
+        ]
+        process = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            env=subprocess_env
+        )
+        return_code = process.returncode
+        result = {
+            'returnCode': return_code, 
+            'stdout': str(process.stdout)
+            }
         return result
 
 api.add_resource(LintRequest, '/lint_request')
