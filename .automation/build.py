85b45b4ed4cc9ccdb4f011a4d8e2f338813a9669
nvuillam <nicolas.vuillamy@gmail.com>
2021-12-27 12:47:54 +0100

    QuickFix

--- .automation/build.py
+++ .automation/build.py
@@ -416,7 +416,7 @@ def generate_linter_dockerfiles():
                 "    CONFIG_REPORTER=false",
                 "",
                 # "EXPOSE 80",
-                "RUN mkdir docker_ssh",
+                "RUN mkdir /root/docker_ssh",
                 "EXPOSE 22",
                 "COPY entrypoint.sh /entrypoint.sh",
                 "RUN chmod +x entrypoint.sh",
