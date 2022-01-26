04069f0952c0bee2d0c9c41f8ab20a5832a8b67c
nvuillam <nicolas.vuillamy@gmail.com>
2022-01-26 23:57:11 +0100

    Use .gitignore as .secretlintignore when necessary

---
+++ megalinter/linters/SecretLintLinter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Use secretlint to find secrets in sources
+https://github.com/secretlint/secretlint
+"""
+
+import os
+from megalinter import Linter
+
+
+class SecretLintLinter(Linter):
+
+    # Called before linting files
+    def before_lint_files(self):
+        # Use .gitignore as .secretlintignore
+        # only if --secretlintignore is not defined and .secretlintignore not found
+        if "--secretlintignore" not in self.cli_lint_user_args and (
+            os.path.isfile(os.path.join(self.workspace, ".gitignore"))
+            and (not os.path.isfile(os.path.join(self.workspace, ".secretlintignore")))
+        ):
+            self.cli_lint_extra_args += ["--secretlintignore", ".gitignore"]
