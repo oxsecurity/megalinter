6fefb14d995cffe888f41aa47058c727ee59bd29
nvuillam <nicolas.vuillamy@gmail.com>
2022-01-27 00:14:02 +0100

    Fix python lint errors

--- megalinter/linters/TfLintLinter.py
+++ megalinter/linters/TfLintLinter.py
@@ -4,10 +4,6 @@ Use TfLint to lint terraform files
 https://github.com/terraform-linters/tflint
 """
 import logging
-import os
-import shutil
-import subprocess
-import sys
 
 import megalinter
 
@@ -17,16 +13,12 @@ class TfLintLinter(megalinter.Linter):
     # To execute before linting files
     def before_lint_files(self):
         # Build pre-command
-        tflint_init_command = 'tflint --init'
+        tflint_init_command = "tflint --init"
         if self.config_file is not None:
-            tflint_init_command += f" --config {self.config_file}" 
+            tflint_init_command += f" --config {self.config_file}"
         logging.debug("tflint before_lint_files: " + tflint_init_command)
         # Add to pre-commands
-        tflint_pre_command = {
-            "command": tflint_init_command,
-            "cwd": self.workspace
-        }
+        tflint_pre_command = {"command": tflint_init_command, "cwd": self.workspace}
         if self.pre_commands is None:
             self.pre_commands = []
         self.pre_commands.append(tflint_pre_command)
-
