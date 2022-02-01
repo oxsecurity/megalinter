4fa1a1f72588e41406c202f50fc07e8f0035e67a
nvuillam <nicolas.vuillamy@gmail.com>
2022-02-01 22:46:45 +0100

    Auto-upgrade pip

--- .automation/build.py
+++ .automation/build.py
@@ -276,7 +276,7 @@ branding:
     pip_install_command = ""
     if len(pip_packages) > 0:
         pip_install_command = (
-            "RUN pip3 install --no-cache-dir --upgrade \\\n          '"
+            "RUN pip3 install --upgrade pip && pip3 install --no-cache-dir --upgrade \\\n          '"
             + "' \\\n          '".join(list(dict.fromkeys(pip_packages)))
             + "'"
         )
