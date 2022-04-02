e5585e9966f3d9e6562f2703726c967fb9366b57
nvuillam <nicolas.vuillamy@gmail.com>
2022-04-02 11:11:50 +0200

    Improve gitleaks

--- megalinter/linters/GitleaksLinter.py
+++ megalinter/linters/GitleaksLinter.py
@@ -9,10 +9,15 @@ from megalinter.utils import is_git_repo
 
 class GitleaksLinter(Linter):
 
-    # Remove --no-git if present
+    # Manage presence of --no-git in command line
     def build_lint_command(self, file=None):
         cmd = super().build_lint_command(file)
-        if '--no-git' in cmd and is_git_repo(self.workspace):
+        # --no-git has been sent by user in REPOSITORY_GITLEAKS_ARGUMENTS
+        # make sure that it is only once in the arguments list
+        if "--no-git" in self.cli_lint_user_args:
+            cmd = list(dict.fromkeys(cmd))
+        # --no-git has been sent by default from ML descriptor
+        # but as it is a git repo, remove all --no-git from arguments list
+        elif "--no-git" in cmd and is_git_repo(self.workspace):
             cmd = list(filter(lambda a: a != "--no-git", cmd))
         return cmd
-
