46a09e533e787e2ce2c0916dc8ad9f9d5aa82409
nvuillam <nvuillam@users.noreply.github.com>
2022-08-23 16:05:09 +0000

    [MegaLinter] Apply linters fixes

--- megalinter/MegaLinter.py
+++ megalinter/MegaLinter.py
@@ -237,7 +237,9 @@ class Megalinter:
                 linter_groups += [linters]
             linter_groups = linter_factory.sort_linters_groups_by_speed(linter_groups)
             # Add "groups" of 1 linter than can not update sources
-            linter_groups += linter_factory.sort_linters_groups_by_speed(linter_groups_without_fixes)
+            linter_groups += linter_factory.sort_linters_groups_by_speed(
+                linter_groups_without_fixes
+            )
         else:
             # If no fixes are applied, we don't care to run same languages linters at the same time
             for linter in active_linters:
