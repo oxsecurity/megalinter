8a3c7ebeec7ab10938c03af20b6ae10033ce99d6
nvuillam <nvuillam@users.noreply.github.com>
2022-10-29 23:15:41 +0000

    [MegaLinter] Apply linters fixes

--- megalinter/tests/test_megalinter/filters_test.py
+++ megalinter/tests/test_megalinter/filters_test.py
@@ -13,7 +13,7 @@ from megalinter.constants import DEFAULT_DOCKER_WORKSPACE_DIR
 class utilsTest(unittest.TestCase):
     def test_file_contains_true(self):
         repo_home = utils.REPO_HOME_DEFAULT
-        regex_list = ["#!/usr/bin/env perl","#!/usr/bin/perl"]
+        regex_list = ["#!/usr/bin/env perl", "#!/usr/bin/perl"]
         regex_object = re.compile("|".join(regex_list), flags=re.MULTILINE)
         file = f"{repo_home}/.automation/test/perl/perl_good_2"
         file_contains_res = utils.file_contains(file, regex_object)
