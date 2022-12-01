8ba59da17014ef38eb551a2393f4906829d49174
Lex <lex.melotti@gmail.com>
2022-12-01 09:39:10 -0300

    Revert some changes for --list-of-files

--- megalinter/linters/DotnetFormatLinter.py
+++ megalinter/linters/DotnetFormatLinter.py
@@ -12,22 +12,10 @@ class DotnetFormatLinter(Linter):
 
     # Build the CLI command to call to lint a file
     def build_lint_command(self, file=None):
-        if file is not None:
-            # cli_lint_mode = file
-            file = os.path.basename(file)
-            dotnet_format_command = " ".join(super().build_lint_command(file))
-            commands = [
-                f'cd "{os.path.realpath(os.path.dirname(file))}" || exit 1',
-                dotnet_format_command + " | tee /dev/tty2 2>&1",
-                'exit "${PIPESTATUS[0]}"',
-            ]
-
-        else:
-            return super().build_lint_command(None)
-            # cli_lint_mode = list_of_files or project
-            # dotnet_format_command = " ".join(super().build_lint_command(None))
-            # commands = [
-            #    dotnet_format_command + " | tee /dev/tty2 2>&1",
-            #    'exit "${PIPESTATUS[0]}"',
-            #]
-        # return " && ".join(commands)
+        commands = [
+            f'cd "{os.path.realpath(os.path.dirname(file))}" || exit 1',
+            " ".join(super().build_lint_command(os.path.basename(file)))
+            + " | tee /dev/tty2 2>&1",
+            'exit "${PIPESTATUS[0]}"',
+        ]
+        return " && ".join(commands)
