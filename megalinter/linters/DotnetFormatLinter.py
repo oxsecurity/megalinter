1e0944af9c2e2aea0a1e965fb913164d65c86f4f
Lex <lex.melotti@gmail.com>
2022-12-01 21:23:40 -0300

    More experiments

--- megalinter/linters/DotnetFormatLinter.py
+++ megalinter/linters/DotnetFormatLinter.py
@@ -12,10 +12,22 @@ class DotnetFormatLinter(Linter):
 
     # Build the CLI command to call to lint a file
     def build_lint_command(self, file=None):
-        commands = [
-            f'cd "{os.path.realpath(os.path.dirname(file))}" || exit 1',
-            " ".join(super().build_lint_command(os.path.basename(file)))
-            + " | tee /dev/tty2 2>&1",
-            'exit "${PIPESTATUS[0]}"',
-        ]
-        return " && ".join(commands)
+        if file is not None:
+            # cli_lint_mode = file
+            file = os.path.basename(file)
+            dotnet_format_command = " ".join(super().build_lint_command(file))
+            commands = [
+                f'cd "{os.path.realpath(os.path.dirname(file))}" || exit 1',
+                dotnet_format_command + " | tee /dev/tty2 2>&1",
+                'exit "${PIPESTATUS[0]}"',
+            ]
+
+        else:
+            return super().build_lint_command(None)
+            # cli_lint_mode = list_of_files or project
+            dotnet_format_command = " ".join(super().build_lint_command(None))
+            commands = [
+                dotnet_format_command + " | tee /dev/tty2 2>&1",
+                'exit "${PIPESTATUS[0]}"',
+            ]
+         return " && ".join(commands)
