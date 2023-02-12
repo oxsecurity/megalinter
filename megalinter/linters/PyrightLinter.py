44b7b75d0b7f9b051d5e3a8701eafe64a906f98f
bdovaz <bdovaz@users.noreply.github.com>
2023-02-12 16:38:57 +0000

    [MegaLinter] Apply linters fixes

--- megalinter/linters/PyrightLinter.py
+++ megalinter/linters/PyrightLinter.py
@@ -5,21 +5,26 @@ https://raku.org/
 """
 import os
 
-from megalinter import Linter
-from megalinter import utils
+from megalinter import Linter, utils
 
 
 class PyrightLinter(Linter):
     def pre_test(self):
         # The file must be in the root of the repository so we create it temporarily for the test.
-        # By default pyright ignores files starting with "." so we override this behavior 
+        # By default pyright ignores files starting with "." so we override this behavior
         # to work with the .automation folder
-        with open(os.path.join(utils.REPO_HOME_DEFAULT, "pyproject.toml"), "w", encoding="utf-8") as f:
-            f.write("""[tool.pyright]
+        with open(
+            os.path.join(utils.REPO_HOME_DEFAULT, "pyproject.toml"),
+            "w",
+            encoding="utf-8",
+        ) as f:
+            f.write(
+                """[tool.pyright]
 exclude = [
     "**/node_modules",
     "**/__pycache__"
-]""")
+]"""
+            )
 
     def post_test(self):
         os.remove(os.path.join(utils.REPO_HOME_DEFAULT, "pyproject.toml"))
