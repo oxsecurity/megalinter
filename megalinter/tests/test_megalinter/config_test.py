49ab2b819eec15bda018a83f61323fb5264d37d0
Borja Domínguez <borja.dominguez@hotmail.com>
2023-02-20 00:14:56 +0100

    Fix tests

--- megalinter/tests/test_megalinter/config_test.py
+++ megalinter/tests/test_megalinter/config_test.py
@@ -3,16 +3,22 @@
 Unit tests for Megalinter class
 
 """
+import glob
 import os
+import re
 import unittest
+
+from git import Repo
 
 from megalinter import config
 from megalinter.constants import ML_REPO
+from megalinter.tests.test_megalinter.helpers import utilstest
 from megalinter.utils import REPO_HOME_DEFAULT
 
 
 class config_test(unittest.TestCase):
     branch = os.environ.get("GITHUB_REF_NAME", "main")
+
     test_folder = (
         f"https://raw.githubusercontent.com/{ML_REPO}/"
         f"{branch}/.automation/test/mega-linter-config-test/"
@@ -34,12 +40,15 @@ class config_test(unittest.TestCase):
         config.delete()
 
     def test_remote_config_success(self):
+        changed_files = self.replace_branch_in_input_files()
         remote_config = self.test_folder + "remote/custom.mega-linter.yml"
         os.environ["MEGALINTER_CONFIG"] = remote_config
         config.init_config()
         self.assertEqual("(custom)", config.get("FILTER_REGEX_INCLUDE"))
+        self.restore_branch_in_input_files(changed_files)
 
     def test_remote_config_error(self):
+        changed_files = self.replace_branch_in_input_files()
         remote_config = self.test_folder + "custom.mega-linter-not-existing.yml"
         try:
             os.environ["MEGALINTER_CONFIG"] = remote_config
@@ -50,8 +59,11 @@ class config_test(unittest.TestCase):
                 r"https://.*/\.automation/test/mega-linter-config-test/"
                 r"custom\.mega-linter-not-existing\.yml"
             ))
+        finally:
+            self.restore_branch_in_input_files(changed_files)
 
     def test_local_config_extends_success(self):
+        changed_files = self.replace_branch_in_input_files()
         local_config = "local.mega-linter.yml"
         os.environ["MEGALINTER_CONFIG"] = local_config
         config.init_config(
@@ -62,11 +74,15 @@ class config_test(unittest.TestCase):
             + "test"
             + os.path.sep
             + "mega-linter-config-test"
+            + os.path.sep
+            + "local_extends"
         )
         self.assertEqual("(local)", config.get("FILTER_REGEX_INCLUDE"))
         self.assertEqual("false", config.get("SHOW_ELAPSED_TIME"))
+        self.restore_branch_in_input_files(changed_files)
 
     def test_local_config_extends_recurse_success(self):
+        changed_files = self.replace_branch_in_input_files()
         local_config = "recurse.mega-linter.yml"
         os.environ["MEGALINTER_CONFIG"] = local_config
         config.init_config(
@@ -84,8 +100,10 @@ class config_test(unittest.TestCase):
         self.assertEqual("false", config.get("SHOW_ELAPSED_TIME"))
         self.assertEqual("dev", config.get("DEFAULT_BRANCH"))
         self.assertEqual("DEBUG", config.get("LOG_LEVEL"))
+        self.restore_branch_in_input_files(changed_files)
 
     def test_local_config_extends_error(self):
+        changed_files = self.replace_branch_in_input_files()
         local_config = "local-error.mega-linter.yml"
         os.environ["MEGALINTER_CONFIG"] = local_config
         try:
@@ -100,24 +118,31 @@ class config_test(unittest.TestCase):
             )
         except Exception as e:
             self.assertIn("No such file or directory", str(e))
+        finally:
+            self.restore_branch_in_input_files(changed_files)
 
     def test_remote_config_extends_success(self):
+        changed_files = self.replace_branch_in_input_files()
         remote_config = self.test_folder + "remote_extends/base.mega-linter.yml"
         os.environ["MEGALINTER_CONFIG"] = remote_config
         config.init_config()
         self.assertEqual("(base)", config.get("FILTER_REGEX_INCLUDE"))
         self.assertEqual("(extension2)", config.get("FILTER_REGEX_EXCLUDE"))
         self.assertEqual("true", config.get("SHOW_ELAPSED_TIME"))
+        self.restore_branch_in_input_files(changed_files)
 
     def test_remote_config_extends_success_2(self):
+        changed_files = self.replace_branch_in_input_files()
         remote_config = self.test_folder + "remote_extends_2/base2.mega-linter.yml"
         os.environ["MEGALINTER_CONFIG"] = remote_config
         config.init_config()
         self.assertEqual("(base)", config.get("FILTER_REGEX_INCLUDE"))
         self.assertEqual("(extension2)", config.get("FILTER_REGEX_EXCLUDE"))
         self.assertEqual("true", config.get("SHOW_ELAPSED_TIME"))
+        self.restore_branch_in_input_files(changed_files)
 
     def test_remote_config_extends_error(self):
+        changed_files = self.replace_branch_in_input_files()
         remote_config = self.test_folder + "remote_extends_error/base-error.mega-linter.yml"
         os.environ["MEGALINTER_CONFIG"] = remote_config
         try:
@@ -125,12 +150,15 @@ class config_test(unittest.TestCase):
             config.init_config()
         except Exception as e:
             self.assertRegex(str(e), (
-                "Unable to retrieve config file "
+                "Unable to retrieve EXTENDS config file "
                 r"https://.*/\.automation/test/mega-linter-config-test/"
                 r"remote_extends_error/base-error\.mega-linter\.yml"
             ))
+        finally:
+            self.restore_branch_in_input_files(changed_files)
 
     def test_local_remote_config_extends_recurse_success(self):
+        changed_files = self.replace_branch_in_input_files()
         local_config = "local.remote.mega-linter.yml"
         os.environ["MEGALINTER_CONFIG"] = local_config
         config.init_config(
@@ -149,6 +177,7 @@ class config_test(unittest.TestCase):
         self.assertEqual("true", config.get("SHOW_ELAPSED_TIME"))
         self.assertEqual("dev", config.get("DEFAULT_BRANCH"))
         self.assertEqual("DEBUG", config.get("LOG_LEVEL"))
+        self.restore_branch_in_input_files(changed_files)
 
     def test_list_of_obj_as_env_var(self):
         os.environ[
@@ -158,3 +187,55 @@ class config_test(unittest.TestCase):
         pre_commands = config.get_list("PRE_COMMANDS", [])
         del os.environ["PRE_COMMANDS"]
         self.assertTrue(len(pre_commands) > 0, "PRE_COMMANDS not loaded from ENV var")
+
+    def replace_branch_in_input_files(self):
+        root = (
+            ".automation"
+            + os.path.sep
+            + "test"
+            + os.path.sep
+            + "mega-linter-config-test"
+        )
+
+        branch = os.environ.get("GITHUB_REF_NAME", "main")
+
+        search_glob_pattern = root.replace("\\", "/") + "/**/*"
+
+        regex = r"(/oxsecurity/megalinter/)(main)(/\.automation)"
+
+        list = []
+
+        for file in glob.iglob(search_glob_pattern, recursive=True):
+            file_name = os.path.basename(file)
+            if (".yml" not in file_name):
+                continue
+
+            match = False
+
+            with open(file, "r", encoding="utf-8") as f:
+                file_content = f.read()
+
+                if re.search(regex, file_content):
+                    file_content = re.sub(
+                        regex,
+                        rf"\1{branch}\3",
+                        file_content
+                    )
+
+                    match = True
+
+            if match:
+                with open(file, "w") as f:
+                    f.write(file_content)
+
+                list.append(file)
+
+        return list
+
+    def restore_branch_in_input_files(self, files):
+        repo = Repo(os.path.realpath(utilstest.REPO_HOME))
+
+        for file in files:
+            repo.index.checkout(
+                [os.path.join(os.path.realpath(utilstest.REPO_HOME), file)], force=True
+            )
