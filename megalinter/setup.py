aed76ac8bb06089797b0136a7548d92e7dcd10ce
nvuillam <nicolas.vuillamy@gmail.com>
2023-03-05 02:30:53 +0100

    Fix flavors import error

--- megalinter/setup.py
+++ megalinter/setup.py
@@ -8,7 +8,7 @@ setup(
     author="Nicolas Vuillamy",
     author_email="nicolas.vuillamy@gmail.com",
     license="MIT",
-    packages=["megalinter", "megalinter.linters", "megalinter.reporters"],
+    packages=["megalinter", "megalinter.linters", "megalinter.reporters", "megalinter.tests"],
     install_requires=[
         "gitpython",
         "jsonpickle",
