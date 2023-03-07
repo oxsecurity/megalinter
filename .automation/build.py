8e856c96ddac37e3a55659ce3fbd6b1d7e8db168
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2023-03-07 00:10:56 -0500

    Update build.py chown npm node_modules

--- .automation/build.py
+++ .automation/build.py
@@ -476,6 +476,7 @@ def build_dockerfile(
             + "    npm doctor || true \\\n"
             + "    && npm audit fix --audit-level=critical || true \\\n"
             + "    && npm cache clean --force || true \\\n"
+            + "    && chown -R $(id -u):$(id -g) node_modules # workaround for https://github.com/npm/cli/issues/5900 \\\n"
             + "    && rm -rf /root/.npm/_cacache \\\n"
             + '    && find . -name "*.d.ts" -delete \\\n'
             + '    && find . -name "*.map" -delete \\\n'
