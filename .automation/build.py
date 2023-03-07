dad8a6cf38220acb1c21143478c3399c05ea28ba
nvuillam <nicolas.vuillamy@gmail.com>
2023-03-07 19:37:51 +0100

    quote dev in omit=dev

--- .automation/build.py
+++ .automation/build.py
@@ -470,8 +470,7 @@ def build_dockerfile(
     if len(npm_packages) > 0:
         npm_install_command = (
             "WORKDIR /node-deps\n"
-            + "# hadolint ignore=SC2046 \\\n"
-            + "RUN npm --no-cache install --ignore-scripts --omit=dev \\\n                "
+            + "RUN npm --no-cache install --ignore-scripts --omit=\"dev\" \\\n                "
             + " \\\n                ".join(list(dict.fromkeys(npm_packages)))
             + "  \\\n"
             + "    && npm audit fix --audit-level=critical || true \\\n"
