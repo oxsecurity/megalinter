d7bb2c9334673409df3d6830e185d2b356a339af
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2023-04-01 21:25:20 -0700

    quick build TEST_KEYWORDS=repository_gitleaks

--- megalinter/linters/GitleaksLinter.py
+++ megalinter/linters/GitleaksLinter.py
@@ -13,48 +13,47 @@ from megalinter.utils import is_git_repo
 
 class GitleaksLinter(Linter):
     def __init__(self, params=None, linter_config=None):
-        self.is_pr, self.pr_source_sha, self.pr_target_sha = self.get_pr_data()
         super().__init__(params, linter_config)
+        self.only_pr_commits = config.get("REPOSITORY_GITLEAKS_ONLY_PR_COMMITS", "false")
+        if self.only_pr_commits == "true":
+            self.is_pr, self.pr_source_sha, self.pr_target_sha = self.get_pr_data()
+
 
     def get_pr_data(self):
         # https://learn.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml
         is_azure_devops_pr = config.get("BUILD_REASON") == "PullRequest"
         azure_devops_pr_source_sha = config.get("BUILD_SOURCEVERSION")
-        azure_devops_pr_target_sha = self.get_pr_target_sha(
-            config.get("SYSTEM_PULLREQUEST_TARGETBRANCHNAME"), is_azure_devops_pr
-        )
+        azure_devops_pr_target_sha = self.get_azure_devops_pr_target_sha(config.get("SYSTEM_PULLREQUEST_TARGETBRANCH"), is_azure_devops_pr)
 
         # https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
         is_github_pr = config.get("GITHUB_EVENT_NAME") == "pull_request"
-        # GITHUB_SHA - commit id of PR
-        # GITHUB_WORKFLOW_SHA - commit id of last commit in PR branch
         github_pr_source_sha, github_pr_target_sha = self.get_github_sha(is_github_pr)
 
         # https://docs.gitlab.com/ee/ci/variables/predefined_variables.html
-        # GitLab merge request
-        is_gitlab_mr = "CI_MERGE_REQUEST_IID" in os.environ
-        gitlab_mr_source_sha = config.get("CI_MERGE_REQUEST_SOURCE_BRANCH_SHA")
-        gitlab_mr_target_sha = self.get_pr_target_sha(
-            config.get("CI_MERGE_REQUEST_TARGET_BRANCH_NAME"), is_gitlab_mr
+        # SHAs are available only on GitLab Premium
+        is_gitlab_premium = (
+            True
+            if config.get("CI_MERGE_REQUEST_EVENT_TYPE") == "merged_result" or config.get("CI_MERGE_REQUEST_EVENT_TYPE") == "merge_train"
+            else False
         )
 
-        # GitLab external PR
-        is_gitlab_external_pr = "CI_EXTERNAL_PULL_REQUEST_IID" in os.environ
-        gitlab_external_pr_source_sha = config.get(
-            "CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_SHA"
-        )
-        gitlab_external_pr_target_sha = self.get_pr_target_sha(
-            config.get("CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_NAME"),
-            is_gitlab_external_pr,
-        )
+        # GitLab merge request
+        is_gitlab_mr = config.get("CI_PIPELINE_SOURCE") == "merge_request_event"
+        gitlab_mr_source_sha = config.get("CI_MERGE_REQUEST_SOURCE_BRANCH_SHA")
+        gitlab_mr_target_sha = config.get("CI_MERGE_REQUEST_TARGET_BRANCH_SHA")
+
+        # GitLab external PR #
+        is_gitlab_external_pr = config.get("CI_PIPELINE_SOURCE") == "external_pull_request_event"
+        gitlab_external_pr_source_sha = config.get("CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_SHA")
+        gitlab_external_pr_target_sha = config.get("CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_SHA")
 
         is_pr = (
             True
             if (
                 is_azure_devops_pr
                 or is_github_pr
-                or is_gitlab_mr
-                or is_gitlab_external_pr
+                or (is_gitlab_mr and is_gitlab_premium)
+                or (is_gitlab_external_pr and is_gitlab_premium)
             )
             else False
         )
@@ -77,11 +76,11 @@ class GitleaksLinter(Linter):
 
         return is_pr, pr_source_sha, pr_target_sha
 
-    def get_pr_target_sha(self, target_branch_name, is_pr=False):
+    def get_azure_devops_pr_target_sha(self, target_branch_name, is_pr=False):
         if not is_pr:
             return None
         repo = Repo(os.path.realpath(self.workspace))
-        return repo.commit(target_branch_name)
+        return repo.commit(target_branch_name.replace("refs/heads", "origin"))
 
     def get_github_sha(self, is_pr=False):
         if not is_pr:
@@ -106,10 +105,11 @@ class GitleaksLinter(Linter):
         elif "--no-git" in cmd and is_git_repo(self.workspace):
             cmd = list(filter(lambda a: a != "--no-git", cmd))
 
-        if config.get("VALIDATE_ALL_CODEBASE") == "false" and self.is_pr:
-            if self.pr_target_sha != self.pr_source_sha:
-                cmd += [
-                    f"--log-opts=--no-merges --first-parent {self.pr_target_sha}^..{self.pr_source_sha}"
-                ]
+        if config.get("VALIDATE_ALL_CODEBASE") == "false" and self.only_pr_commits == "true":
+            if self.is_pr:
+                if self.pr_target_sha != self.pr_source_sha:
+                    cmd += [
+                        f"--log-opts={self.pr_target_sha}^..{self.pr_source_sha}"
+                    ]
 
         return cmd
