8fc4d8202b209ba5b401a3395ab3c9c274ee08ff
Aaron Aichlmayr <aaichlmayr@conquestcyber.com>
2023-04-19 15:09:47 -0500

    Made the dockerfules look cleaner

--- .automation/build.py
+++ .automation/build.py
@@ -666,7 +666,7 @@ def build_dockerfile(
             pipenv_path_command += f":/venvs/{pip_linter}/bin"
         pipenv_download_command += (
             '&& pip download --cache-dir=/var/cache/pip --dest "/download" \\\n      '
-            + ("\\\n      ".join(pipenv_download_list))
+            + (" \\\n      ".join(pipenv_download_list))
             + " \\\n"
         )
         pipenv_install_command = pipenv_install_command[:-2]  # remove last \
