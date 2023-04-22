892e5f5c8e2e4520d164c4ba64cfdbcfb739f762
nvuillam <nicolas.vuillamy@gmail.com>
2023-04-22 16:17:18 +0200

    Return more info

--- megalinter/reporters/WebHookLinterReporter.py
+++ megalinter/reporters/WebHookLinterReporter.py
@@ -9,6 +9,7 @@ import os
 
 import requests
 from megalinter import Reporter, config
+from megalinter.constants import ML_DOC_URL_DESCRIPTORS_ROOT
 
 
 class WebHookLinterReporter(Reporter):
@@ -20,7 +21,7 @@ class WebHookLinterReporter(Reporter):
     def __init__(self, params=None):
         # Deactivate GitHub Status by default
         self.is_active = False
-        self.processing_order = 20 # Run after text reporter
+        self.processing_order = 20  # Run after text reporter
         super().__init__(params)
 
     def manage_activation(self):
@@ -37,13 +38,15 @@ class WebHookLinterReporter(Reporter):
     def produce_report(self):
         success_msg = "No errors were found in the linting process"
         error_not_blocking = "Errors were detected but are considered not blocking"
-        error_msg = f"Found {self.master.total_number_errors}, please check logs"
+        error_msg = f"Found {self.master.total_number_errors} errors"
         headers = {
             "accept": "application/json",
             "content-type": "application/json",
         }
         if config.exists("WEBHOOK_REPORTER_BEARER_TOKEN"):
-            headers[ "authorization"] = f"Bearer {config.get('WEBHOOK_REPORTER_BEARER_TOKEN')}"
+            headers[
+                "authorization"
+            ] = f"Bearer {config.get('WEBHOOK_REPORTER_BEARER_TOKEN')}"
         status_message = (
             success_msg
             if self.master.status == "success" and self.master.return_code == 0
@@ -51,22 +54,34 @@ class WebHookLinterReporter(Reporter):
             if self.master.status == "error" and self.master.return_code == 0
             else error_msg
         )
+        lang_lower = self.master.descriptor_id.lower()
+        linter_name_lower = self.master.linter_name.lower().replace("-", "_")
+        linter_doc_url = (
+            f"{ML_DOC_URL_DESCRIPTORS_ROOT}/{lang_lower}_{linter_name_lower}"
+        )
         data = {
             "status": "success" if self.master.return_code == 0 else "error",
+            "errorNumber": self.master.total_number_errors,
             "statusMessage": status_message,
             "elapsedTime": round(self.master.elapsed_time_s, 2),
-            "megalinterDescriptorId": self.master.descriptor_id,
-            "megalinterLinterId": self.master.linter_name,
+            "descriptorId": self.master.descriptor_id,
+            "linterId": self.master.linter_name,
+            "docUrl": linter_doc_url,
+            "isFormatter": self.master.is_formatter
         }
         if (
             self.master.sarif_output_file is not None
             and os.path.isfile(self.master.sarif_output_file)
             and os.path.getsize(self.master.sarif_output_file) > 0
         ):
-            with open(self.master.sarif_output_file, "r", encoding="utf-8") as linter_sarif_file:
+            with open(
+                self.master.sarif_output_file, "r", encoding="utf-8"
+            ) as linter_sarif_file:
                 data["outputSarif"] = json.load(linter_sarif_file)
         else:
-            text_report_sub_folder = config.get("TEXT_REPORTER_SUB_FOLDER", "linters_logs")
+            text_report_sub_folder = config.get(
+                "TEXT_REPORTER_SUB_FOLDER", "linters_logs"
+            )
             text_file_name = (
                 f"{self.report_folder}{os.path.sep}"
                 f"{text_report_sub_folder}{os.path.sep}"
