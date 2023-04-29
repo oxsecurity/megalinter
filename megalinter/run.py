3c4c1a7f94bcbf5c4cc2ffb4f04bdc832d70d505
nvuillam <nicolas.vuillamy@gmail.com>
2023-04-29 18:17:44 +0200

    Default request id

--- megalinter/run.py
+++ megalinter/run.py
@@ -5,8 +5,12 @@ Run mega-linter
 """
 
 import megalinter
+import uuid
 
-linter = megalinter.Megalinter({"cli": True})
+linter = megalinter.Megalinter({
+    "cli": True,
+    "request_id": uuid.uuid1()
+})
 
 # Guess who's there ? :)
 megalinter.alpaca()
