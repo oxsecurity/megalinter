8ac99045e92bc140a13580dded1341e5d228623e
nvuillam <nicolas.vuillamy@gmail.com>
2023-05-01 17:05:21 +0200

    Fix

--- megalinter/Linter.py
+++ megalinter/Linter.py
@@ -1148,7 +1148,7 @@ class Linter:
             self.cli_lint_fix_arg_name is not None
             or str(self.cli_executable_fix) != str(self.cli_executable)
         ):
-            cmd.pop(len(self.cli_executable)) 
+            cmd.pop(len(self.cli_executable) - 1) # Remove executable elements
             cmd = self.cli_executable_fix + cmd
             cmd += [self.cli_lint_fix_arg_name]
             self.try_fix = True
