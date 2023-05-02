ab59d78a2d6ab4f55abb65006e6735d5419aeb64
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2023-05-01 23:31:35 -0700

    update config.get

--- megalinter/linters/GitleaksLinter.py
+++ megalinter/linters/GitleaksLinter.py
@@ -12,7 +12,7 @@ import megalinter.utils as utils
 class GitleaksLinter(Linter):
     def __init__(self, params=None, linter_config=None):
         super().__init__(params, linter_config)
-        self.pr_commits_scan = config.get("REPOSITORY_GITLEAKS_PR_COMMITS_SCAN", "false")
+        self.pr_commits_scan = config.get(self.request_id, "REPOSITORY_GITLEAKS_PR_COMMITS_SCAN", "false")
         if self.pr_commits_scan == "true" and utils.is_pr():
             self.pr_source_sha, self.pr_target_sha = self.get_pr_data()
 
@@ -24,28 +24,28 @@ class GitleaksLinter(Linter):
         # GitLab ref:
         # https://docs.gitlab.com/ee/ci/variables/predefined_variables.html
 
-        pr_source_sha = config.get("REPOSITORY_GITLEAKS_PR_SOURCE_SHA")
-        pr_target_sha = config.get("REPOSITORY_GITLEAKS_PR_TARGET_SHA")
+        pr_source_sha = config.get(self.request_id, "REPOSITORY_GITLEAKS_PR_SOURCE_SHA")
+        pr_target_sha = config.get(self.request_id, "REPOSITORY_GITLEAKS_PR_TARGET_SHA")
 
         if pr_source_sha is None or pr_target_sha is None:
             if utils.is_azure_devops_pr():
                 # SYSTEM_PULLREQUEST_SOURCECOMMITID -> SHA of the last commit of the PR
                 # BUILD_SOURCEVERSION -> SHA of the PR intermediate merge branch
-                pr_source_sha = config.get("SYSTEM_PULLREQUEST_SOURCECOMMITID")
+                pr_source_sha = config.get(self.request_id, "SYSTEM_PULLREQUEST_SOURCECOMMITID")
                 # SYSTEM_PULLREQUEST_TARGETBRANCH -> name of the target branch, e.g. refs/heads/main
-                pr_target_sha = self.get_azure_devops_pr_target_sha(config.get("SYSTEM_PULLREQUEST_TARGETBRANCH"))
+                pr_target_sha = self.get_azure_devops_pr_target_sha(config.get(self.request_id, "SYSTEM_PULLREQUEST_TARGETBRANCH"))
             elif utils.is_github_pr():
                 pr_source_sha, pr_target_sha = self.get_github_sha()
             elif utils.is_gitlab_mr() and utils.is_gitlab_premium():
                 # CI_MERGE_REQUEST_SOURCE_BRANCH_SHA -> SHA of the last commit of the PR
-                pr_source_sha = config.get("CI_MERGE_REQUEST_SOURCE_BRANCH_SHA")
+                pr_source_sha = config.get(self.request_id, "CI_MERGE_REQUEST_SOURCE_BRANCH_SHA")
                 # CI_MERGE_REQUEST_TARGET_BRANCH_SHA -> SHA of the last commit in the target branch
-                pr_target_sha = config.get("CI_MERGE_REQUEST_TARGET_BRANCH_SHA")
+                pr_target_sha = config.get(self.request_id, "CI_MERGE_REQUEST_TARGET_BRANCH_SHA")
             elif utils.is_gitlab_external_pr() and utils.is_gitlab_premium():
                 # CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_SHA -> SHA of the last commit of the PR
-                pr_source_sha = config.get("CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_SHA")
+                pr_source_sha = config.get(self.request_id, "CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_SHA")
                 # CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_SHA -> SHA of the last commit in the target branch
-                pr_target_sha = config.get("CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_SHA")
+                pr_target_sha = config.get(self.request_id, "CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_SHA")
 
         return pr_source_sha, pr_target_sha
 
@@ -54,7 +54,7 @@ class GitleaksLinter(Linter):
         return repo.commit(target_branch_name.replace("refs/heads", "origin"))
 
     def get_github_sha(self):
-        gh_event_file = open(config.get("GITHUB_EVENT_PATH"))
+        gh_event_file = open(config.get(self.request_id, "GITHUB_EVENT_PATH"))
         gh_event = json.load(gh_event_file)
         gh_event_file.close()
         return (
@@ -77,7 +77,7 @@ class GitleaksLinter(Linter):
         if "--no-git" in cmd and utils.is_git_repo(self.workspace):
             cmd = list(filter(lambda a: a != "--no-git", cmd))
 
-        if config.get("VALIDATE_ALL_CODEBASE") == "false" and self.pr_commits_scan == "true" and utils.is_pr():
+        if config.get(self.request_id, "VALIDATE_ALL_CODEBASE") == "false" and self.pr_commits_scan == "true" and utils.is_pr():
             if (self.pr_target_sha is not None
                 and self.pr_source_sha is not None
                 and self.pr_target_sha != self.pr_source_sha):
