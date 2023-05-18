a58df66db9c13916eec7355a1928b81463ba0210
nvuillam <nicolas.vuillamy@gmail.com>
2023-05-18 12:16:25 +0200

    Manage permissions

--- megalinter/reporters/GithubCommentReporter.py
+++ megalinter/reporters/GithubCommentReporter.py
@@ -139,13 +139,10 @@ class GithubCommentReporter(Reporter):
                 except github.GithubException as e:
                     logging.warning(
                         f"[GitHub Comment Reporter] Unable to post pull request comment: {str(e)}.\n"
-                        "To enable this function, please :\n"
-                        "1. Create a Personal Access Token (https://docs.github.com/en/free-pro-team@"
-                        "latest/github/authenticating-to-github/creating-a-personal-access-token)\n"
-                        "2. Create a secret named PAT with its value on your repository (https://docs."
-                        "github.com/en/free-pro-team@latest/actions/reference/encrypted-secrets#"
-                        "creating-encrypted-secrets-for-a-repository)"
-                        "3. Define PAT={{secrets.PAT}} in your GitHub action environment variables"
+                        "To enable this function, please add permissions in your Github Actions Workflow:\n"
+                        "permissions:\n"
+                        "  issues: write\n"
+                        "  pull-requests: write"
                     )
                 except Exception as e:
                     logging.warning(
