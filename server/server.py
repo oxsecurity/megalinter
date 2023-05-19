66db382964ae72ab2c2ff503bd4725c965b506f1
nvuillam <nicolas.vuillamy@gmail.com>
2023-05-19 23:41:48 +0200

    Docker compose tryouts

--- server/server.py
+++ server/server.py
@@ -2,6 +2,8 @@
 https://medium.com/@mike.p.moritz/using-docker-compose-to-deploy-a-lightweight-python-rest-api-with-a-job-queue-37e6072a209b
 Run server: REDIS_HOST=localhost uvicorn megalinter.server:app (needs a redis server online on localhost:6379)
 Run worker: cd /mnt/c/git/megalinter && rq worker --url redis://localhost:6379 megalinter_queue
+
+docker build -t megalinter-server:latest -f server/Dockerfile .
 '''
 import logging
 import logging.config
