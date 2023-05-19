81d50b43fe5d02ccd4698c9acde5ca71ae1f9d01
nvuillam <nicolas.vuillamy@gmail.com>
2023-05-19 17:08:20 +0200

    Fix plugins table

--- .automation/build.py
+++ .automation/build.py
@@ -1796,12 +1796,12 @@ def build_plugins_md_table():
         plugins_file_data = yaml.safe_load(f)
     plugins = plugins_file_data["plugins"]
     md_table = [
-        "| <!-- --> | Name | Description | Author | Raw URL |",
-        "| :------: | :----- | :---------- | :--------------: | :--- |",
+        "| Name | Description | Author | Raw URL |",
+        "| :----- | :---------- | :--------------: | :--- |",
     ]
     for plugin in plugins:
         md_table += [
-            f"| <!-- icon --> | [{plugin['name']}]({plugin['docUrl']}) | "
+            f"| [{plugin['name']}]({plugin['docUrl']}) | "
             f"{plugin['description']} | {plugin['author']} | "
             f"[Descriptor]({plugin['pluginUrl']}) |",
         ]
