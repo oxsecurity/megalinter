e0a7f5b8c73136d23aa6e27606f8e1779e811b47
nvuillam <nicolas.vuillamy@gmail.com>
2023-05-19 22:48:56 +0200

    init

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from .server_worker import processAnalysisRequest
+from .types import AnalysisRequestInput,AnalysisRequestOutput,AnalysisStatus
 
 __all__ = [
     "server",
     "server_worker",
+    "types",
     processAnalysisRequest
 ]
