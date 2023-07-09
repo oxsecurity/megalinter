2c2f9be99c1d74322ee8afe57f5eb8ce5a90fea6
nvuillam <nicolas.vuillamy@ox.security>
2023-07-09 20:54:56 +0200

    try fix terraform test files

--- .automation/test/terraform/good/terraform_good_2.tf
+++ .automation/test/terraform/good/terraform_good_2.tf
@@ -1,15 +1,14 @@
-terraform {
-  required_version = ">= 0.15.0"
+required_version = ">= 0.15.0"
 
-  resource "aws_instance" "good" {
-    ami                         = "ami-0ff8a91507f77f867"
-    instance_type               = "t2.small"
-    associate_public_ip_address = false
+resource "aws_instance" "good" {
+  ami                         = "ami-0ff8a91507f77f867"
+  instance_type               = "t2.small"
+  associate_public_ip_address = false
 
-    vpc_security_group_ids = ["sg-12345678901234567"]
+  vpc_security_group_ids = ["sg-12345678901234567"]
 
-    ebs_block_device {
-      encrypted = true
-    }
+  ebs_block_device {
+    encrypted = true
   }
 }
+
