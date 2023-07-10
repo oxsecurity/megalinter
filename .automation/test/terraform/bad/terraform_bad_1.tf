3ec77cf59ba7d7a1f99f506394eeec49fd9ff649
nvuillam <nicolas.vuillamy@ox.security>
2023-07-10 08:16:39 +0200

    new tests

--- .automation/test/terraform/bad/terraform_bad_1.tf
+++ .automation/test/terraform/bad/terraform_bad_1.tf
@@ -1,7 +1,12 @@
+terraform {
+  required_version = ">= 1.2.5"
+}
+
 resource "aws_instance" "bad" {
   instance_type               = "t2.small"
   associate_public_ip_address = false
 
+  murf = "cupcake8"
 
   ebs_block_device {
     encrypted = true
--- .automation/test/terraform/bad/terraform_bad_2.tf
+++ .automation/test/terraform/bad/terraform_bad_2.tf
@@ -1,8 +1,14 @@
+terraform {
+  required_version = ">= 1.2.5"
+}
+
 resource "aws_instance" "bad" {
   ami                         = "ami-0ff8a91507f77f867"
   associate_public_ip_address = false
 
   vpc_security_group_ids = ["sg-12345678901234567"]
+
+  murf = "cupcake"
 
   ebs_block_device {
     encrypted = true
