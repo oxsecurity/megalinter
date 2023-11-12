ea2051ffa7cfe86d60e1825ac9c3038404a642f7
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2023-11-12 11:45:34 -0500

    Fix flake8 error (line too long) in constants.py

--- megalinter/constants.py
+++ megalinter/constants.py
@@ -34,5 +34,6 @@ DEFAULT_DOCKERFILE_APK_PACKAGES = [
     "make",
     "musl-dev",
     "openssh",
-    "openssl", # Temporary, to make sure openssl 3.1.3 that is installed by default gets replaced by 3.1.4 for CVE-2023-5363 (2023-11-12)
+    # Temporary, to make sure openssl 3.1.3 (installed by default) is replaced by 3.1.4 for CVE-2023-5363 (2023-11-12)
+    "openssl",
 ]
