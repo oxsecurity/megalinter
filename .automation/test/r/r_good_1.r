8029dc6425c228571ce57c4fa1f0092f66b31c4e
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2023-11-12 11:10:54 -0500

    Update .lintr config in template and tests to address depreciation, and sync bad test file with newest lintr default test file

--- .automation/test/r/r_good_1.r
+++ .automation/test/r/r_good_1.r
@@ -2,7 +2,7 @@
 
 # assignment
 # function_left_parentheses
-# closed_curly
+# brace_linter
 # commas
 # paren_brace
 f <- function(x, y = 1) {
@@ -13,12 +13,15 @@ f <- function(x, y = 1) {
 
 # cyclocomp
 # equals_na
+# brace_linter
+# indentation
 # infix_spaces
 # line_length
 # object_length
 # object_name
 # object_usage
 # open_curly
+# T_and_F_symbol
 short_snake <- function(x) {
   y <- 1
   y <- y^2
@@ -29,9 +32,19 @@ short_snake <- function(x) {
   }
 }
 
+# vector_logic
+if (1 & 2) FALSE else TRUE
+
+# function_brace
+my_metric <- function(x) {
+  sum(x) + prod(x)
+}
+
+# no_tab
 # pipe_continuation
 # seq_linter
 # spaces_inside
+# indentation
 x <- 1:10
 x[2]
 seq_len(x) %>%
@@ -43,6 +56,8 @@ message("single_quotes")
 
 # spaces_left_parentheses
 # trailing_whitespace
+# semicolon
+x <- 42
 y <- 2 + (1:10)
 
 # trailing_blank_lines
