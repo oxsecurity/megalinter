d30eec1a78ac6d21e127a982c4441e241b6d2c43
nvuillam <nvuillam@users.noreply.github.com>
2024-04-30 19:48:04 +0000

    [MegaLinter] Apply linters fixes

--- .automation/build.py
+++ .automation/build.py
@@ -423,7 +423,7 @@ def build_dockerfile(
     docker_arg_top = []
     docker_arg_main = []
     for docker_arg_item in docker_arg:
-        match = re.match(r'ARG\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*=?\s*', docker_arg_item)
+        match = re.match(r"ARG\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*=?\s*", docker_arg_item)
         arg_name = match.group(1)
         if arg_name in all_from_instructions:
             docker_arg_top += [docker_arg_item]
