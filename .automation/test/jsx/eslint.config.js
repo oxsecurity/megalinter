a0042c44e5c990ac63c2222e32dfef55817f049a
nvuillam <nicolas.vuillamy@gmail.com>
2024-09-07 02:51:29 +0200

    don't use globals

--- .automation/test/jsx/eslint.config.js
+++ .automation/test/jsx/eslint.config.js
@@ -1,11 +1,7 @@
 const react = require('eslint-plugin-react');
-const globals = require('globals');
 
 module.exports = {
     files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-    globals: {
-        ...globals.browser,
-      },
     extends: [
         "eslint:recommended",
         "plugin:react/recommended"
