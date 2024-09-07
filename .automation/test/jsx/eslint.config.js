7ee2188f2c00be8697e2c24f136cb0e1695f87ec
nvuillam <nicolas.vuillamy@gmail.com>
2024-09-07 10:09:44 +0200

    try again

--- .automation/test/jsx/eslint.config.js
+++ .automation/test/jsx/eslint.config.js
@@ -1,21 +1,5 @@
-const react = require('eslint-plugin-react');
+const reactPlugin = require('eslint-plugin-react');
 
-module.exports = {
-    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 12,
-        sourceType: "module"
-    },
-    plugins: [
-        react
-    ],
-    rules: {
-    }
-}
+module.exports = [
+  reactPlugin.configs.flat.recommended, // This is not a plugin object, but a shareable config object
+];
