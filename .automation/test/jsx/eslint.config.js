0a6b7161c5deeda161e288f0fcc432670501812f
nvuillam <nicolas.vuillamy@gmail.com>
2024-09-07 10:53:38 +0200

    try again again

--- .automation/test/jsx/eslint.config.js
+++ .automation/test/jsx/eslint.config.js
@@ -1,5 +1,8 @@
 const reactPlugin = require('eslint-plugin-react');
 
 module.exports = [
-  reactPlugin.configs.flat.recommended, // This is not a plugin object, but a shareable config object
+    {
+        files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+        ...reactPlugin.configs.flat.recommended, // This is not a plugin object, but a shareable config object
+    }
 ];
