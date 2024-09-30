45cf565b9e37f72ff16dec8d9d3ac94dde51b9c9
nvuillam <nvuillam@users.noreply.github.com>
2024-09-30 11:28:04 +0000

    [MegaLinter] Apply linters fixes

--- megalinter/linters/TrivyLinter.py
+++ megalinter/linters/TrivyLinter.py
@@ -12,9 +12,9 @@ class TrivyLinter(Linter):
     counter = 0
 
     def execute_lint_command(self, command):
-        return_code, return_output  = super().execute_lint_command(command)
+        return_code, return_output = super().execute_lint_command(command)
         if "TOOMANYREQUESTS" in return_output and self.counter < 10:
             logging.info("[Trivy] Hit TOOMANYREQUESTS: try again")
             self.counter = self.counter + 1
-            return_code, return_output  = self.execute_lint_command(command)
+            return_code, return_output = self.execute_lint_command(command)
         return return_code, return_output
