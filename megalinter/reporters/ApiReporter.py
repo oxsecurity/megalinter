oxsecurity/megalinter

commit 162ec541b9b0eae93a03ade96552b6049b08fccd

[automation] Auto-update linters version, help and documentation (#4073)
* [automation] Auto-update linters version, help and documentation

* [MegaLinter] Apply linters fixes

* Fix ApiReporter activation

* Build

---------

Co-authored-by: nvuillam <nvuillam@users.noreply.github.com>

megalinter/reporters/ApiReporter.py
@@ -56,6 +56,7 @@ class ApiReporter(Reporter):
                     "[ApiReporter] You need to define API_REPORTER_URL to use ApiReporter"
                 )
         else:
+            self.is_active = False
             logging.info(
                 "[ApiReporter] Not enabled as API_REPORTER is not defined to true"
             )

commit 9d0831e9385e2b0b3833a36528822dccdaff3e53

Update stale comment
After a recent change in the code, a comment preceding a changed line
needed an update.

Fixes: 162ec541b9 ("[automation] Auto-update linters version, help and documentation (#4073)")
Signed-off-by: Roger Luethi <roger.luethi@bisdn.de>

megalinter/reporters/ApiReporter.py
@@ -29,7 +29,7 @@ class ApiReporter(Reporter):
     TRUNCATE_LOKI_CHARS_LENGTH = 5000
 
     def __init__(self, params=None):
-        # Deactivate Api reporter by default
+        # Activate Api reporter by default
         self.is_active = True
         self.processing_order = 20  # Run after text reporter
         super().__init__(params)