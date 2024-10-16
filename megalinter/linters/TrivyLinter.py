69e1f9dfcf48f4b85d4115547640e7748a29c17a
nvuillam <nvuillam@users.noreply.github.com>
2024-10-16 17:19:17 +0000

    [MegaLinter] Apply linters fixes

--- megalinter/linters/TrivyLinter.py
+++ megalinter/linters/TrivyLinter.py
@@ -14,7 +14,9 @@ class TrivyLinter(Linter):
 
     def execute_lint_command(self, command):
         return_code, return_output = super().execute_lint_command(command)
-        if ("TOOMANYREQUESTS" in return_output) or ("failed to download Java DB" in return_output):
+        if ("TOOMANYREQUESTS" in return_output) or (
+            "failed to download Java DB" in return_output
+        ):
             # Try 5 times
             if self.counter < 5:
                 time.sleep(3.0)
