c9ae8572d6cf60deb55253720eaba5a9c8ea2d3b
Luke Lloyd <luke.lloyd1985@gmail.com>
2024-10-22 19:20:47 +0100

    apply_fixes typo correction

--- megalinter/reporters/UpdatedSourcesReporter.py
+++ megalinter/reporters/UpdatedSourcesReporter.py
@@ -61,12 +61,11 @@ class UpdatedSourcesReporter(Reporter):
         if len(updated_files) > 0:
             logging.info(
                 f"[Updated Sources Reporter] copied {str(len(updated_files))} fixed source files"
-                f" in folder {updated_sources_dir}.\n"
-                "Download it from artifacts then copy-paste it in your local repo to apply linters updates"
+                f" in folder {updated_sources_dir}."
             )
 
             if not config.exists(self.master.request_id, "GITHUB_REPOSITORY"):
-                apply_fixes = config.get_list(self.master.request_id, "APPLY_FIXES", "none")
+                apply_fixes = config.get(self.master.request_id, "APPLY_FIXES", "none")
                 if apply_fixes.lower() != "none":
                     try:
                         repo = git.Repo(os.path.realpath(self.master.github_workspace))
@@ -76,6 +75,7 @@ class UpdatedSourcesReporter(Reporter):
                     except Exception as exp:
                         logging.error(
                             "[Updated Sources Reporter] Failed to git push auto fixes: " + str(exp.message)
+                            "\nDownload it from artifacts then copy-paste it in your local repo to apply linters updates"
                         )
         else:
             logging.info(
