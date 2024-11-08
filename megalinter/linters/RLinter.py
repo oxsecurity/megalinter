304e37e050760bb852ccd73aa556ac72d242ef58
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-11-08 15:18:40 -0800

    Fix logic for user-supplied config files

--- megalinter/linters/RLinter.py
+++ megalinter/linters/RLinter.py
@@ -12,26 +12,20 @@ class RLinter(Linter):
     # Build the CLI command to call to lint a file
     def build_lint_command(self, file=None):
         # Build command in R format
-
         r_commands = [
             # Change the working directory to match the file
             f"setwd('{Path(file).parent}')",
+            # Instruct lintr to walk up the directory tree
+            f"lintr:::read_settings('{self.config_file_name}')",
+            f"lints <- lintr::lint('{Path(file).name}')",
+            "print(lints)",
+            "quit(save = 'no', status = if (length(lints) > 0) 1 else 0)",
         ]
 
         if self.config_file:
             # Instruct lintr to load an absolute filepath
-            r_commands.append(f"options('lintr.linter_file' = '{self.config_file}')")
-        else:
-            # Instruct lintr to walk up the directory tree
-            r_commands.append(f"lintr:::read_settings('{self.config_file_name}')")
+            r_commands.insert(0, f"options('lintr.linter_file' = '{self.config_file}')")
 
-        r_commands.extend(
-            [
-                f"lints <- lintr::lint('{Path(file).name}')",
-                "print(lints)",
-                "quit(save = 'no', status = if (length(lints) > 0) 1 else 0)",
-            ]
-        )
         # Build shell command
         cmd = ["R", "--slave", "-e", ";".join(r_commands)]
         return cmd
