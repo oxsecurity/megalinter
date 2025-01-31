ceeed3f6e9bb428bbeb56750002ab2ec367addb0
nvuillam <nicolas.vuillamy@gmail.com>
2025-01-31 09:05:08 +0100

    Allow cspell to work with CLI_LINT_MODE=project
    Fixes https://github.com/oxsecurity/megalinter/issues/4622

--- megalinter/linters/CSpellLinter.py
+++ megalinter/linters/CSpellLinter.py
@@ -51,6 +51,13 @@ class CSpellLinter(Linter):
                     "[cspell] Unable to check file names on a readonly workspace: "
                     + str(e)
                 )
+        # Add . parameter if cli_lint_mode is project
+        if (
+            self.cli_lint_mode == "project"
+            and "." not in self.cli_lint_extra_args_after
+            and "." not in self.cli_lint_extra_args
+        ):
+            self.cli_lint_extra_args_after.append(".")
         return super().build_lint_command(file)
 
     # Remove temp file with file names if existing
