27d203507f16526ad9f54b078eac592bf6cfd2de
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-02-06 18:00:05 -0500

    Revert "Configure renovate for apk, gem, cargo, pip and npm dependencies (#4588)"
    This reverts commit 0c8eebfd23b6f5cb969c5aba115ab5d582a6b127.

--- megalinter/Linter.py
+++ megalinter/Linter.py
@@ -1251,10 +1251,7 @@ class Linter:
             lambda arg, w=workspace_value: arg.replace("{{WORKSPACE}}", w),
             self.cli_docker_args,
         )
-        docker_command += [
-            f"{self.cli_docker_image}:"
-            + f"{os.environ.get(self.cli_docker_image_version, self.cli_docker_image_version)}"
-        ]
+        docker_command += [f"{self.cli_docker_image}:{self.cli_docker_image_version}"]
         if isinstance(command, str):
             command = " ".join(docker_command) + " " + command
         else:
