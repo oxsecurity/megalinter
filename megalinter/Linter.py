9ea473aafa6bab856cccfff22b7bd639cf7b58fb
Borja Domínguez Vázquez <borja.dominguez@hotmail.com>
2025-03-07 00:41:02 +0100

    wip

--- megalinter/Linter.py
+++ megalinter/Linter.py
@@ -1522,16 +1522,17 @@ class Linter:
             for run in sarif_output["runs"]:
                 rule_default_level_map = {}
 
-                for rule in run["tool"]["driver"]["rules"]:
-                    if "defaultConfiguration" in rule and "level" in rule["defaultConfiguration"]:
-                        rule_default_level_map[rule["id"]] = rule["defaultConfiguration"]["level"]
+                if "rules" in run["tool"]["driver"]:
+                    for rule in run["tool"]["driver"]["rules"]:
+                        if "defaultConfiguration" in rule and "level" in rule["defaultConfiguration"]:
+                            rule_default_level_map[rule["id"]] = rule["defaultConfiguration"]["level"]
 
                 for result in run["results"]:
                     count_results = False
 
                     if "level" in result:
-                        # Check if the level matches. Otherwise, if the level to be checked is "warning", counts them as if they were
-                        # to cover "note" and "none" levels
+                        # Check if the level matches. Otherwise, if the level to be checked is "warning",
+                        # counts them as if they were to cover "note" and "none" levels
                         # https://docs.oasis-open.org/sarif/sarif/v2.1.0/errata01/os/sarif-v2.1.0-errata01-os-complete.html#_Toc141790898
                         if result["level"] == level or level == "warning":
                             count_results = True
