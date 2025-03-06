31ae48152cd190125de8c651d89b40ea14a05270
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2025-03-06 00:18:53 +0000

    Add workaround for markdownlint-cli2 returning exit code 2 with `--help`

--- megalinter/utilstest.py
+++ megalinter/utilstest.py
@@ -416,9 +416,13 @@ def test_get_linter_help(linter, test_self):
     # Check linter help
     help_txt = linter.get_linter_help()
     print("[" + linter.linter_name + "] help: " + help_txt)
-    # Ugly workaround to avoid instability of get sql_tsqllint_test version
-    if help_txt == "ERROR" and test_self.__class__.__name__ == "sql_tsqllint_test":
-        raise unittest.SkipTest("Ugly workaround to avoid sql_tsqllint_test failure")
+    if help_txt == "ERROR":
+        # Ugly workaround to avoid instability of get sql_tsqllint_test version
+        if test_self.__class__.__name__ == "sql_tsqllint_test":
+            raise unittest.SkipTest("Ugly workaround to avoid sql_tsqllint_test failure")
+        # Workaround for markdownlint-cli2 returning exit code 2 with --help
+        if test_self.__class__.__name__ == "markdown_markdownlint_cli2_test":
+            raise unittest.SkipTest("Workaround for markdownlint-cli2 exit code 2 with --help")
     test_self.assertFalse(
         help_txt == "ERROR", "Returned help invalid: [" + help_txt + "]"
     )
