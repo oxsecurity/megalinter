c2f89dc30facd3646e44f227122d23f550f5bfe1
Borja Domínguez <iam.n3uro@gmail.com>
2025-04-30 00:07:50 +0200

    wip

---
+++ megalinter/linters/ReviveLinter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Use Revive to lint go files
+"""
+import os
+
+from megalinter import Linter, config
+
+
+class ReviveLinter(Linter):
+    def pre_test(self, test_name):
+        if test_name == "test_report_sarif":
+            config.set_value(
+                self.request_id, "GO_REVIVE_CONFIG_FILE", "revive_bad.toml"
+            )
