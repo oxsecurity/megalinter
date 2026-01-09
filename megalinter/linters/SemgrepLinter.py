d624acdb48276d783294f7f007a9eee237a0bc89
bdovaz <iam.n3uro@gmail.com>
2026-01-09 22:36:39 +0100

    wip

--- megalinter/linters/SemgrepLinter.py
+++ megalinter/linters/SemgrepLinter.py
@@ -6,7 +6,7 @@ Use SemGrep to lint any type of file according to local config
 import logging
 import os
 
-from megalinter import Linter, config, flavor_factory, utils
+from megalinter import Linter, config, flavor_factory, utils, utilstest
 
 
 class SemgrepLinter(Linter):
@@ -77,16 +77,18 @@ class SemgrepLinter(Linter):
         return []
 
     def pre_test(self, test_name):
-        # The file must be in the root of the repository so we create it temporarily for the test.
-        # By default semgrep ignores files with "test" so we override this behavior
-        # to work with the .automation folder
-        # https://semgrep.dev/docs/ignoring-files-folders-code#override-defaults
-        with open(
-            os.path.join(os.getcwd(), ".semgrepignore"),
-            "w",
-            encoding="utf-8",
-        ) as f:
-            f.write("""""")
+        if test_name == "test_failure" or test_name == "test_report_sarif":
+            # The file must be in the root of the repository so we create it temporarily for the test.
+            # By default semgrep ignores files with "test" so we override this behavior
+            # to work with the .automation/test folder
+            # https://semgrep.dev/docs/ignoring-files-folders-code#override-defaults
+            with open(
+                os.path.join(utilstest.get_root_dir(), ".semgrepignore"),
+                "w",
+                encoding="utf-8",
+            ) as f:
+                f.write("")
 
     def post_test(self, test_name):
-        os.remove(os.path.join(os.getcwd(), ".semgrepignore"))
+        if test_name == "test_failure" or test_name == "test_report_sarif":
+            os.remove(os.path.join(utilstest.get_root_dir(), ".semgrepignore"))
