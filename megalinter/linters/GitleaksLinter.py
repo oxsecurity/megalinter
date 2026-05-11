d47058dfd23f960e9acab3be5ab923a7cc9ae09c
bdovaz <iam.n3uro@gmail.com>
2026-05-11 21:03:54 +0200

    wip

--- megalinter/linters/GitleaksLinter.py
+++ megalinter/linters/GitleaksLinter.py
@@ -125,3 +125,7 @@ class GitleaksLinter(Linter):
                 cmd += self.cli_lint_extra_args
 
         return cmd
+
+    def pre_test(self, test_name):
+        if test_name.endswith("file_lint_mode") or test_name.endswith("list_of_files_lint_mode"):
+            config.set_value(self.request_id, "REPOSITORY_GITLEAKS_FILE_EXTENSIONS", [".txt"])
---
+++ megalinter/linters/GrypeLinter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Use GrypeLinter to lint Infrastructure as Code
+"""
+
+from megalinter import Linter, config
+
+
+class GrypeLinter(Linter):
+    def build_lint_command(self, file=None):
+        cmd = super().build_lint_command(file)
+
+        if (self.cli_lint_mode == "file"):
+            cmd.remove(file)
+
+        return cmd
+
+    def pre_test(self, test_name):
+        if test_name.endswith("file_lint_mode") or test_name.endswith("list_of_files_lint_mode"):
+            config.set_value(self.request_id, "REPOSITORY_GRYPE_FILE_EXTENSIONS", [".json"])
