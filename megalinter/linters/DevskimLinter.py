0ff3a9f6152e2fdfa885bdf659395a4090228cc5
Borja Domínguez <iam.n3uro@gmail.com>
2026-05-11 11:16:14 +0200

    wip

--- megalinter/linters/DevskimLinter.py
+++ megalinter/linters/DevskimLinter.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-Use Checkov to lint Infrastructure as Code
+Use Devskim to lint Infrastructure as Code
 """
 
 from megalinter import Linter, config
 
 
-class CheckovLinter(Linter):
+class DevskimLinter(Linter):
     def pre_test(self, test_name):
         if test_name.endswith("file_lint_mode") or test_name.endswith("list_of_files_lint_mode"):
-            config.set_value(self.request_id, "REPOSITORY_CHECKOV_FILE_EXTENSIONS", [".tf"])
+            config.set_value(self.request_id, "REPOSITORY_DEVSKIM_FILE_EXTENSIONS", [".sh"])
