cbbd544b981c2ec7e1745dc7655362c54c31e24e
bdovaz <iam.n3uro@gmail.com>
2026-05-15 09:10:09 +0200

    wip

--- megalinter/tests/test_megalinter/linter_test.py
+++ megalinter/tests/test_megalinter/linter_test.py
@@ -16,20 +16,15 @@ class LinterTest(unittest.TestCase):
         linter.report_folder = "test_report_folder"
         linter.workspace = "test_workspace"
 
-        args = [
-            "{{SARIF_OUTPUT_FILE}}",
-            "{{REPORT_FOLDER}}",
-            "{{WORKSPACE}}"
-        ]
+        args = ["{{SARIF_OUTPUT_FILE}}", "{{REPORT_FOLDER}}", "{{WORKSPACE}}"]
         additional_variables = None
 
         replaced_args = linter.replace_vars(args, additional_variables)
 
-        self.assertEqual([
-            "test_sarif_output_file",
-            "test_report_folder",
-            "test_workspace"
-        ], replaced_args)
+        self.assertEqual(
+            ["test_sarif_output_file", "test_report_folder", "test_workspace"],
+            replaced_args,
+        )
 
     def test_replace_vars_with_unknown_variable(self):
         linter = Linter.__new__(Linter)
@@ -37,16 +32,12 @@ class LinterTest(unittest.TestCase):
         linter.report_folder = "test_report_folder"
         linter.workspace = "test_workspace"
 
-        args = [
-            "{{UNKNOWN_VAR}}"
-        ]
+        args = ["{{UNKNOWN_VAR}}"]
         additional_variables = None
 
         replaced_args = linter.replace_vars(args, additional_variables)
 
-        self.assertEqual([
-            "{{UNKNOWN_VAR}}"
-        ], replaced_args)
+        self.assertEqual(["{{UNKNOWN_VAR}}"], replaced_args)
 
     def test_replace_vars_with_additional_variables(self):
         linter = Linter.__new__(Linter)
@@ -54,15 +45,9 @@ class LinterTest(unittest.TestCase):
         linter.report_folder = "test_report_folder"
         linter.workspace = "test_workspace"
 
-        args = [
-            "{{ADDITIONAL_VAR}}"
-        ]
-        additional_variables = {
-            "{{ADDITIONAL_VAR}}": "test_additional_var"
-        }
+        args = ["{{ADDITIONAL_VAR}}"]
+        additional_variables = {"{{ADDITIONAL_VAR}}": "test_additional_var"}
 
         replaced_args = linter.replace_vars(args, additional_variables)
 
-        self.assertEqual([
-            "test_additional_var"
-        ], replaced_args)
+        self.assertEqual(["test_additional_var"], replaced_args)
